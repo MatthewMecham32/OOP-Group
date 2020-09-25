4e89557245cd2e107fede700b4095c33eded43e4
MatthewMecham32 <70896344+MatthewMecham32@users.noreply.github.com>
2020-09-25 08:31:36 -0600

    Add files via upload

---
+++ oopgroup.py
@@ -0,0 +1,19 @@
+class Customer() :
+    def __init__(self, sCustID, sCustFirst, sCustLast,sAddress1, sAddress2,sCity,sState,sZip, fBalance, sCompanyName, sCustPet,) :
+        self.sCustID = sCustID
+        self.sCustFirstName = sCustFirst
+        self.sCustLastName = sCustLast
+        self.sAddress1 = sAddress1
+        self.sAddress2 = sAddress2
+        self.iAccountNumber = iAccountNumber
+        self.fBalance = fBalance
+
+
+sCustFirst = ""
+sCustLast = ""
+sAddress1 = ""
+sAddress2 = ""
+sCity = ""
+sState = ""
+sZip = ""
+fBalance = ""
